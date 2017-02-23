5363c6352d983ba65c7686a08c8aeb6558411905
Gabriel Jucht <gabrieljucht@gmail.com>
2017-02-22 21:13:57 -0300

    banner etc

--- wp-content/themes/colormag/header.php
+++ wp-content/themes/colormag/header.php
@@ -110,15 +110,20 @@ wp_head();
 						</div><!-- #header-text -->
 					</div><!-- #header-left-section -->
 					<div id="header-right-section">
-						<?php
-						if( is_active_sidebar( 'colormag_header_sidebar' ) ) {
-						?>
-						<div id="header-right-sidebar" class="clearfix">
-             <!-- Banner Header -->
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <!-- Banner Header -->
           <ins class="adsbygoogle"
               style="display:inline-block;width:728px;height:90px"
               data-ad-client="ca-pub-5206566395845628"
               data-ad-slot="5669698395"></ins>
+          <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+						<?php
+						if( is_active_sidebar( 'colormag_header_sidebar' ) ) {
+						?>
+						<div id="header-right-sidebar" class="clearfix">
+           
 						<?php
 							// Calling the header sidebar if it exists.
 							if ( !dynamic_sidebar( 'colormag_header_sidebar' ) ):
